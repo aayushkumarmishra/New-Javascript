c884425c0b7e26afaa68cd9a03bc73439d470f05
Ayush Kumar Mishra <ayushmishra9615@gmail.com>
2023-09-27 21:14:23 +0530

    javascript variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const account_Id = 134555;
+let accountEmail = "ayushmishra9615@gmail.com"
+var accountPassword = "12345"
+accountCity = "Varanasi"
+let accountState;
+
+
+// account_Id = 2 // Not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "54321"
+accountCity = "Delhi"
+
+
+console.log(account_Id);
+console.table([account_Id,accountEmail, accountPassword, accountCity, accountState])
+
+/* 
+    Prefer Not use to var
+    because of issue in block scope and functional scope
+*/
